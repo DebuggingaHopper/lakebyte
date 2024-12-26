56ab105bc9bd0d5a3e3209ec46ef08d27dbbe06c
debuggingahopper <nelson.j.alvarez01@gmail.com>
2024-12-26 18:02:21 -0500

    Minor adjustment to sidebar

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -7,9 +7,11 @@ import {
   import { IconButton } from "@radix-ui/themes";
 const Sidebar: React.FC = () => {
   return (
-    <div className="min-h-screen px-4 pt-8 pb-0 bg-[#003a51] flex justify-between flex-col w-60">
-
-<div className="space-y-3 h-full flex items-center flex-col p-3 justify-center items-stretch fixed">
+    <div className="min-h-screen px-4 pt-8 pb-0 bg-[#003a51] flex justify-between flex-col w-60 hidden lg:block">
+<div className="space-y-1 h-full flex flex-col w-30 text-wrap text-base p-30 fixed items-stretch font-bold divide-y">
+<p className="text-[#ffffff] text-wrap">
+  Nelson Alvarez's DevOps Blog
+</p>
           <a href="https://www.linkedin.com/in/nelson-alvarez-62027b189" className="text-[#ffffff] hover:text-[#fff6a2]">
             <IconButton>
               <LinkedInLogoIcon width="40" height="40" />
