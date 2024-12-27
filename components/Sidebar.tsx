7862be2de8411afe56ff19577b663b48726e6355
debuggingahopper <nelson.j.alvarez01@gmail.com>
2024-12-27 13:07:20 -0500

    Finalized the sidebar for the webpage

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -7,26 +7,27 @@ import {
   import { IconButton } from "@radix-ui/themes";
 const Sidebar: React.FC = () => {
   return (
-    <div className="min-h-screen px-4 pt-8 pb-0 bg-[#003a51] flex justify-between flex-col w-60 hidden lg:block">
-<div className="space-y-1 h-full flex flex-col w-30 text-wrap text-base p-30 fixed items-stretch font-bold divide-y">
-<p className="text-[#ffffff] text-wrap">
-  Nelson Alvarez's DevOps Blog
+    <div className="min-h-screen px-4 pt-8 pb-0 bg-[#003a51] flex justify-between flex-col w-60 hidden fixed lg:block">
+<div className="min-h-screen space-y-1 h-full flex flex-col justify-center items-center w-30 text-wrap text-center text-base p-30">
+<img src="/assets/pfp.jpg" className="w-40 h-40 rounded-full" alt="PFP picture"></img>
+<p className="text-[#ffffff] font-semibold">
+  Nelson Alvarez's DevOps Blog about optimizing niche problems
 </p>
-          <a href="https://www.linkedin.com/in/nelson-alvarez-62027b189" className="text-[#ffffff] hover:text-[#fff6a2]">
+          <a href="https://www.linkedin.com/in/nelson-alvarez-62027b189" className="text-[#ffffff] hover:text-[#fff6a2] font-bold">
             <IconButton>
               <LinkedInLogoIcon width="40" height="40" />
             </IconButton>
               LinkedIn
           </a>
 
-          <a href="https://github.com/DebuggingaHopper" className="text-[#ffffff] hover:text-[#fff6a2]">
+          <a href="https://github.com/DebuggingaHopper" className="text-[#ffffff] hover:text-[#fff6a2] font-bold">
             <IconButton>
               <GitHubLogoIcon width="40" height="40" />
             </IconButton>
               Github
           </a>
 
-          <a href="mailto:“nelson.j.alvarez01@gmail.com" className="text-[#ffffff] hover:text-[#fff6a2]">
+          <a href="mailto:“nelson.j.alvarez01@gmail.com" className="text-[#ffffff] hover:text-[#fff6a2] font-bold">
             <IconButton>
               <EnvelopeOpenIcon width="40" height="40" />
             </IconButton>
