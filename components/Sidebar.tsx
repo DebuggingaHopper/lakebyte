8537241a81d611761e4242ff5684eee269851133
debuggingahopper <nelson.j.alvarez01@gmail.com>
2024-12-27 20:17:51 -0500

    minor addition to sidebar

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -8,10 +8,10 @@ import {
 const Sidebar: React.FC = () => {
   return (
     <div className="min-h-screen px-4 pt-8 pb-0 bg-[#003a51] flex justify-between flex-col w-60 hidden fixed lg:block">
-<div className="min-h-screen space-y-1 h-full flex flex-col justify-center items-center w-30 text-wrap text-center text-base p-30">
+<div className="min-h-screen space-y-3 h-full flex flex-col justify-center items-center w-30 text-wrap text-center text-base p-30">
 <img src="/assets/pfp.jpg" className="w-40 h-40 rounded-full" alt="PFP picture"></img>
 <p className="text-[#ffffff] font-semibold">
-  Nelson Alvarez's DevOps Blog about optimizing niche problems
+  Nelson Alvarez's DevOps Blog about optimization & development
 </p>
           <a href="https://www.linkedin.com/in/nelson-alvarez-62027b189" className="text-[#ffffff] hover:text-[#fff6a2] font-bold">
             <IconButton>
